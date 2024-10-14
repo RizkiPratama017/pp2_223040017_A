5d651705d46fd878eb684e1ccd501ba32b847a9f
RizkiPratama017 <rizki.223040017@mail.unpas.ac.id>
2024-10-14 08:52:35 +0700

    Revisi sesi 4

--- src/sesi4/WindowListenerExample.java
+++ src/sesi4/WindowListenerExample.java
@@ -4,41 +4,41 @@ import javax.swing.*;
 
 public class WindowListenerExample {  
     public static void main(String[] args) {  
-        JFrame f = new JFrame("Window Listener Example");  
+        JFrame frame = new JFrame("Window Listener Example");  
         
         JLabel label = new JLabel("lakukan operasi pada jendela");
         label.setBounds(50,50,300, 30);
 
-        f.addWindowListener(new WindowListener() { 
+        frame.addWindowListener(new WindowListener() { 
 
-            public void windowOpened(WindowEvent e) {  
+            public void windowOpened(WindowEvent e) {    //ketika dibuka
                 label.setText("Window Opened");  
             }  
-            public void windowClosing(WindowEvent e) {  
-                label.setText("Window Closing");  
+            public void windowClosing(WindowEvent e) {   //ketika proses penutupan
+                label.setText("Window Closing");  //bisa ditambahkan system.exit(0); jika ingin menghentikan program
         
             }  
-            public void windowClosed(WindowEvent e) {  
+            public void windowClosed(WindowEvent e) {  //benar benar ditutup
                 System.out.println("Window Closed");  
             }  
-            public void windowIconified(WindowEvent e) {  
+            public void windowIconified(WindowEvent e) {  //minimaze
                 label.setText("Window Minimized");  
             }  
-            public void windowDeiconified(WindowEvent e) {  
+            public void windowDeiconified(WindowEvent e) {  //full setelah minimaze
                 label.setText("Window Restored");  
             }  
-            public void windowActivated(WindowEvent e) {  
+            public void windowActivated(WindowEvent e) {  //menjadi aktif (berfokus)
                 label.setText("Window Activated");  
             }  
-            public void windowDeactivated(WindowEvent e) {  
+            public void windowDeactivated(WindowEvent e) {  //kehilangan fokus
                 label.setText("Window Deactivated");  
             }  
         });  
         
-        f.add(label);
-        f.setSize(400, 400);  
-        f.setLayout(null);  
-        f.setVisible(true);  
-        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.add(label);
+        frame.setSize(400, 400);  
+        frame.setLayout(null);  
+        frame.setVisible(true);  
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }  
 }
